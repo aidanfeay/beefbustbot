b2fa398856606a106fbd1467bf6a2f15e6d33d6f
Aidan Feay <aidanlfeay@gmail.com>
2015-08-14 00:37:14 -0400

    temp fix for duplicate tweets

--- bbb.js
+++ bbb.js
@@ -20,19 +20,21 @@ function coinflip() {
 
 var text,
 		screen_name,
-		id_str
+		id_str,
+		tweets,
 		hashtag = 'BeefOrBustBot';
 
 setInterval(function() {
   bot.twit.get('search/tweets', { q: '%23' + hashtag + ' since:2011-11-11', count: 1 }, function(err, data, response) {
 		if (err) return handleError(err);
 		if (data.statuses[0] != undefined){
-			if (id_str == data.statuses[0].id_str) {
+			if (tweets.indexOf(data.statuses[0].id_str) > -1) {
 				console.log('Sorry, duplicate tweet...');
 			} else {
 				id_str = data.statuses[0].id_str;
 				text = data.statuses[0].text;
 				screen_name = data.statuses[0].user.screen_name;
+				tweets.push(id_str);
 				var tweet = coinflip() + '! https://twitter.com/' + screen_name + '/status/' + id_str;
 				bot.twit.post('statuses/update', { status: tweet }, function (err, data, response) {
 				  console.log('Posted: ' + tweet);
